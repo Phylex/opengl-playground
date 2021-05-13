675d7c96ae3b6e231dc45b2d1e72fae1a9405793
Alexande Becker <becker.alexander42@gmail.com>
2021-05-13 14:42:55 +0200

    changed the code to use 'modern' opengl

--- p2_opengl_playground.cpp
+++ p2_opengl_playground.cpp
@@ -30,6 +30,21 @@ int main(int argc, char **argv) {
 	
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	// the following block is the initialisation and copying of the data into
+	// the memory space of the OpenGL implementation
+	float positions[] = {
+		-.5f, -.5f,
+		-.0f, .5f,
+		.5f, -.5f,
+	}
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	// the following function needs to know the buffer
+	// size in bytes or a null 
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glBegin(GL_TRIANGLES);
