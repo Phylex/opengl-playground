9f7a5c87915986eb5777273400ae2ead0e805ef5
Alexande Becker <becker.alexander42@gmail.com>
2021-06-05 13:13:26 +0200

    changed away from the cherno stuff for the first tutorial

--- p1_opengl_playground.cpp
+++ p1_opengl_playground.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
-#define PROJECT_NAME "Opengl-playground"
+#define PROJECT_NAME "Opengl-playground part 1"
 
 int main(int argc, char **argv) {
     if(argc != 1) {
@@ -10,36 +11,39 @@ int main(int argc, char **argv) {
         return 1;
     }
     printf("This is project %s.\n", PROJECT_NAME);
-	GLFWwindow* window;
+	glewExperimental = true;
 
-
-	if (!glfwInit())
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
 		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 1);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "Part 1", NULL, NULL);
 	if (!window) {
+		std::cerr << "Failed to open GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(window);
 
 	// the documentation specifies that glew can only be initialized after a valid
 	// rendering context has been enabled
-	if (glewInit() != GLEW_OK)
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) {
 		std::cout << "GLEW initialisation failed!" << std::endl;
-	
+		return -1;
+	}
 	std::cout << glGetString(GL_VERSION) << std::endl;
-
-	while (!glfwWindowShouldClose(window)) {
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	do {
 		glClear(GL_COLOR_BUFFER_BIT);
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-.5f, -.5f);
-		glVertex2f(-.0f, .5f);
-		glVertex2f(.5f, -.5f);
-		glEnd();
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-	}
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 	glfwTerminate();
     return 0;
 }
